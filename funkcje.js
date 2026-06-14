bba183654ae817cd9e4abd0c9e172145fe6e73fe
rafjur1303-droid <rafjur1303@gmail.com>
2026-06-14 21:15:53 +0200

    Add files via upload

--- funkcje.js
+++ funkcje.js
@@ -1,3 +1,98 @@
+//=====(ZAPIS)=====
+function saveGame() {
+    let save = {
+        //=====(ZMIENNE DO ZAPISU)=====
+        money: money,
+        multiplier: multiplier,
+        rebirth: rebirth,
+        superRebirth: superRebirth,
+        up1Count: up1Count,
+        up1Cost: up1Cost,
+        up1Boost: up1Boost,
+        up2Count: up2Count,
+        up2Cost: up2Cost,
+        up2Boost: up2Boost,
+        up2Active: up2Active,
+        up3Count: up3Count,
+        up3Cost: up3Cost,
+        up3Boost: up3Boost,
+        up4Count: up4Count,
+        up4Cost: up4Cost,
+        up4Boost: up4Boost,
+        up4Active: up4Active,
+        playTime: playTime,
+        up5Count: up5Count,
+        up5Cost: up5Cost,
+        up5Boost: up5Boost,
+        up5Active: up5Active,
+        up6Count: up6Count,
+        up6Cost: up6Cost,
+        up6Boost: up6Boost,
+        up7Count: up7Count,
+        up7Cost: up7Cost,
+        up7Boost: up7Boost,
+        up8Count: up8Count,
+        up8Cost: up8Cost,
+        up8Boost: up8Boost,
+        up8Active: up8Active,
+        up9Count: up9Count,
+        up9Active: up9Active,
+        up9Cost : up9Cost,
+    };
+    localStorage.setItem("mojaGraSave", JSON.stringify(save));
+    console.log("Gra zapisana!");
+}
+//=====(WCZYTYWANIE)=====
+function loadGame() {
+    let savedData = localStorage.getItem("mojaGraSave");
+    if (savedData) {
+        let save = JSON.parse(savedData);
+        //=====(ZMIENNE DO WCZYTYWANIA)=====
+        money = save.money;
+        multiplier = save.multiplier;
+        rebirth = save.rebirth;
+        superRebirth = save.superRebirth;
+        up1Count = save.up1Count;
+        up1Cost = save.up1Cost;
+        up1Boost = save.up1Boost;
+        up2Count = save.up2Count;
+        up2Cost = save.up2Cost;
+        up2Boost = save.up2Boost;
+        up2Active = save.up2Active;
+        up3Count = save.up3Count;
+        up3Cost = save.up3Cost;
+        up3Boost = save.up3Boost;
+        up4Count = save.up4Count;
+        up4Cost = save.up4Cost;
+        up4Boost = save.up4Boost;
+        up4Active = save.up4Active;
+        playTime = save.playTime;
+        up5Count = save.up5Count;
+        up5Cost = save.up5Cost;
+        up5Boost = save.up5Boost;
+        up5Active = save.up5Active;
+        up6Count = save.up6Count;
+        up6Cost = save.up6Cost;
+        up6Boost = save.up6Boost;
+        up7Count = save.up7Count;
+        up7Cost = save.up7Cost;
+        up7Boost = save.up7Boost;
+        up8Count = save.up8Count;
+        up8Cost = save.up8Cost;
+        up8Boost = save.up8Boost;
+        up8Active = save.up8Active;
+        up9Count = save.up9Count;
+        up9Active = save.up9Active;
+        up9Cost = save.up9Cost;
+        console.log("Gra wczytana!");
+    }
+}
+//=====(HARD RESET)=====
+function resetGame() {
+    localStorage.removeItem("mojaGraSave");
+    localStorage.removeItem("gameTheme");
+    location.reload();
+}
 //=====(ZMIANA MENU)=====
 function changeMainTab(tabName) {
     document.getElementById('sub-menu-stats').style.display = 'none';
@@ -78,7 +173,7 @@ function stopAutoBuyMultiplier() {
 function buyMultiplier() {
     if (money >= 100) {
         money -= 100;
-        multiplier += 1 * (rebirth + 1);
+        multiplier += (rebirth + 1) * up2Boost * up3Boost * up7Boost;
         updateUI();
     }
 }
@@ -86,7 +181,7 @@ function buyMultiplier() {
 function buyRebirth() {
     if (multiplier >= 100) {
         money = 0;
-        let gain = round(((Math.log10(multiplier + 1.01) - 1) ** 2) * (superRebirth + 1));
+        let gain = round(((Math.log10(multiplier + 1.01) - 1) ** 2) * (superRebirth + 1) * up4Boost * up5Boost * up7Boost);
         if (isFinite(gain) && gain > 0) rebirth += gain;
         multiplier = 0;
         updateUI();
@@ -97,7 +192,7 @@ function buySuperRebirth() {
     if (rebirth >= 100) {
         money = 0;
         multiplier = 0;
-        let gain = round(((Math.log10(rebirth + 1.01) - 1) ** 2.2));
+        let gain = round(((Math.log10(rebirth + 1.01) - 1) ** 2.2) * up6Boost * up7Boost * up8Boost);
         if (isFinite(gain) && gain > 0) superRebirth += gain;
         rebirth = 0;
         updateUI();
@@ -105,6 +200,19 @@ function buySuperRebirth() {
 }
 //=====(AUTO KASA)=====
 setInterval(() => {
-    money += (multiplier + 1);
+    money += (multiplier + 1) * up1Boost * up7Boost;
+    updateUI();
+}, 100);
+//=====(AUTO ZAPIS)=====
+setInterval(() => {
+    saveGame();
+}, 5000);
+//=====(WCZYTYWANIE GRY)=====
+window.onload = function() {
+    loadGame();
+}
+//=====(CZAS GRY)=====
+setInterval(() => {
+    playTime += 0.1
     updateUI();
 }, 100);
--- zmienne.js
+++ zmienne.js
@@ -3,4 +3,47 @@ let money = 0;
 let multiplier = 0;
 let rebirth = 0;
 let superRebirth = 0;
-
+//=====(UBGRADE 1)=====
+let up1Count = 0;
+let up1Cost = 10000;
+let up1Boost = 1;
+let up1Cap = 10;
+//=====(UBGRADE 2)=====
+let up2Count = 0;
+let up2Cost = 250000;
+let up2Active = false;
+let up2Boost = 1;
+//=====(UBGRADE 3)=====
+let up3Count = 0;
+let up3Cost = 250;
+let up3Boost = 1;
+let up3Cap = 10;
+//=====(UBGRADE 4)=====
+let up4Count = 0;
+let up4Cost = 4000;
+let up4Active = false;
+let up4Boost = 1;
+let playTime = 0;
+//=====(UBGRADE 5)=====
+let up5Count = 0;
+let up5Cost = 20;
+let up5Active = false;
+let up5Boost = 1;
+//=====(UBGRADE 6)=====
+let up6Count = 0;
+let up6Cost = 125;
+let up6Boost = 1;
+let up6Cap = 25;
+//=====(UBGRADE 7)=====
+let up7Count = 0;
+let up7Cost = 8;
+let up7Boost = 1;
+//=====(UBGRADE 8)=====
+let up8Count = 0;
+let up8Cost = 25;
+let up8Active = false;
+let up8Boost = 1;
+//=====(UBGRADE 9)=====
+let up9Count = 0;
+let up9Cost = 125;
+let up9Active = false;
